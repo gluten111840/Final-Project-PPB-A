59344e4e30b6d7e01671ec419f9b1135d40891e9
fitrah1812 <arieramadhanfitrah@gmail.com>
2021-12-20 12:52:01 +0700

    add : success ocr

--- app/src/main/java/com/ppb/appkasir/OcrActivity.java
+++ app/src/main/java/com/ppb/appkasir/OcrActivity.java
@@ -6,18 +6,32 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import java.util.List;
+
 
 public class OcrActivity extends AppCompatActivity {
 
     private Button captureImageBtn, detectTextBtn;
     private ImageView imageView;
     private TextView textView;
+    Bitmap imageBitmap;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -36,12 +50,19 @@ public class OcrActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dispatchTakePictureIntent();
+                textView.setText("");
             }
         });
         tutup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+        detectTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                detectTextFromImage();
             }
         });
     }
@@ -57,8 +78,38 @@ public class OcrActivity extends AppCompatActivity {
         super.onActivityResult(requestCode,resultCode,data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageBitmap = (Bitmap) extras.get("data");
             imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void detectTextFromImage(){
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();;
+        firebaseVisionTextDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                displayTextFromImage(firebaseVisionText);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(OcrActivity.this, "Udin", Toast.LENGTH_SHORT).show();
+                Log.d("Error: ", e.getMessage());
+            }
+        });
+    }
+
+    private void displayTextFromImage(FirebaseVisionText firebaseVisionText) {
+        List<FirebaseVisionText.Block> blockList = firebaseVisionText.getBlocks();
+        if(blockList.size() == 0){
+            Toast.makeText(this, "No Text Found in image", Toast.LENGTH_SHORT);
+        }
+        else{
+            for(FirebaseVisionText.Block block: firebaseVisionText.getBlocks()){
+                String text = block.getText();
+                textView.setText(text);
+            }
         }
     }
 }
