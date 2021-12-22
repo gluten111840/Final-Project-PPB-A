94ba297b23daa8ae691ff0031d573480cdee910a
Bayu Eka Prawira <bayueka980@gmail.com>
2021-12-22 17:43:02 +0700

    refactor: change from realtime database to firestore firebase

--- app/src/main/java/com/ppb/appkasir/Adapter/ProdukDataAdapter.java
+++ app/src/main/java/com/ppb/appkasir/Adapter/ProdukDataAdapter.java
@@ -3,11 +3,16 @@ import de.codecrafters.tableview.*;
 import android.view.*;
 import java.util.*;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.ppb.appkasir.MainActivity;
 import com.ppb.appkasir.Model.*;
@@ -23,8 +28,9 @@ import com.ppb.appkasir.Model.Produk;
 
 public class ProdukDataAdapter extends TableDataAdapter
 {
-	FirebaseDatabase firebaseDatabase;
-	DatabaseReference databaseReference;
+//	FirebaseDatabase firebaseDatabase;
+//	DatabaseReference databaseReference;
+	FirebaseFirestore myDB;
 	public static final NumberFormat PRICE_FORMATTER = NumberFormat.getNumberInstance();
 	public ProdukDataAdapter(Context ctx, ArrayList<Produk> prod){
 		super(ctx, prod);
@@ -58,50 +64,98 @@ public class ProdukDataAdapter extends TableDataAdapter
 		return pos;
 	}
 	public void tambah(ContentValues val){
-		Produk product = new Produk(val.getAsString("nama"), val.getAsString("sn"), val.getAsLong("harga"), val.getAsInteger("stok"));
+//		Produk product = new Produk(val.getAsString("nama"), val.getAsString("sn"), val.getAsLong("harga"), val.getAsInteger("stok"));
 		String sn = val.getAsString("sn");
 		getData().add(new Produk(val.getAsString("nama"), val.getAsString("sn"), val.getAsLong("harga"), val.getAsInteger("stok")));
+		Map<String, Object> data = new HashMap<>();
+		data.put("nama", val.getAsString("nama"));
+		data.put("sn", val.getAsString("sn"));
+		data.put("harga", val.getAsLong("harga"));
+		data.put("stok", val.getAsInteger("stok"));
 		new DBHelper(getContext()).tambah(val);
-		firebaseDatabase = FirebaseDatabase.getInstance();
-		// on below line creating our database reference.
-		databaseReference = firebaseDatabase.getReference("Produk");
-		databaseReference.addValueEventListener(new ValueEventListener() {
-			@Override
-			public void onDataChange(@NonNull DataSnapshot snapshot) {
-				// on below line we are setting data in our firebase database.
-				databaseReference.child(sn).setValue(product);
-
-			}
-
-			@Override
-			public void onCancelled(@NonNull DatabaseError error) {
-				// TODO
-			}
-		});
+		myDB = FirebaseFirestore.getInstance();
+		myDB.collection("Produk")
+				.document(sn)
+				.set(data);
+//		firebaseDatabase = FirebaseDatabase.getInstance();
+//		// on below line creating our database reference.
+//		databaseReference = firebaseDatabase.getReference("Produk");
+//		databaseReference.addValueEventListener(new ValueEventListener() {
+//			@Override
+//			public void onDataChange(@NonNull DataSnapshot snapshot) {
+//				// on below line we are setting data in our firebase database.
+//				databaseReference.child(sn).setValue(product);
+//
+//			}
+//
+//			@Override
+//			public void onCancelled(@NonNull DatabaseError error) {
+//				// TODO
+//			}
+//		});
 
 		notifyDataSetChanged();
 	}
 	public void hapus(Produk produk){
 		getData().remove(produk);
-		String sn = produk.getSn();
-		firebaseDatabase = FirebaseDatabase.getInstance();
-		// on below line we are initialing our database reference and we are adding a child as our course id.
-		databaseReference = firebaseDatabase.getReference("Produk").child(sn);
-		// on below line calling a method to delete the course.
-		databaseReference.removeValue();
+//		String sn = produk.getSn();
 		new DBHelper(getContext()).delete(produk.getSn());
+		myDB = FirebaseFirestore.getInstance();
+		myDB.collection("Produk").document(produk.getSn()).delete()
+				.addOnSuccessListener(new OnSuccessListener<Void>() {
+					@Override
+					public void onSuccess(Void aVoid) {
+//						toastResult("Data deleted successfully");
+					}
+				})
+				.addOnFailureListener(new OnFailureListener() {
+					@Override
+					public void onFailure(@NonNull Exception e) {
+//						toastResult("Error while deleting the data : " + e.getMessage());
+					}
+				});
+//		firebaseDatabase = FirebaseDatabase.getInstance();
+//		// on below line we are initialing our database reference and we are adding a child as our course id.
+//		databaseReference = firebaseDatabase.getReference("Produk").child(sn);
+//		// on below line calling a method to delete the course.
+//		databaseReference.removeValue();
 		notifyDataSetChanged();
 	}
 	public void perbarui(Produk produk, ContentValues newdata){
-		Produk product = new Produk(newdata.getAsString("nama"), newdata.getAsString("sn"), newdata.getAsLong("harga"), newdata.getAsInteger("stok"));
+//		String sn = produk.getSn();
+//		firebaseDatabase = FirebaseDatabase.getInstance();
+//		databaseReference = firebaseDatabase.getReference("Produk").child(sn);
 		int idx=getpos(produk);//getData().indexOf(produk);
 		if(idx>=0){
 			
 		}else{
 			return;
 		}
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("harga", produk.getHarga());
+//		map.put("nama", produk.getNama());
+//		map.put("sn", produk.getSn());
+//		map.put("stok", produk.getStok());
+//		databaseReference.addValueEventListener(new ValueEventListener() {
+//			@Override
+//			public void onDataChange(@NonNull DataSnapshot snapshot) {
+//				// adding a map to our database.
+//				databaseReference.updateChildren(map);
+//			}
+//
+//			@Override
+//			public void onCancelled(@NonNull DatabaseError error) {
+//			}
+//		});
 		getData().set(idx, new Produk(newdata.getAsString("nama"), newdata.getAsString("sn"), newdata.getAsLong("harga"), newdata.getAsInteger("stok")));
 		new DBHelper(getContext()).update(newdata, produk.getSn());
+		myDB = FirebaseFirestore.getInstance();
+		Map<String, Object> data = new HashMap<>();
+		data.put("nama", newdata.getAsString("nama"));
+		data.put("sn", newdata.getAsString("sn"));
+		data.put("harga", newdata.getAsLong("harga"));
+		data.put("stok", newdata.getAsInteger("stok"));
+		myDB.collection("Produk").document(produk.getSn()).update(data);
 		notifyDataSetChanged();
 	}
 	private View renderString(final String value) {
